c370d4d9a09dc03c64667bd98f6abfc068653370
Joseph Omotade <josephomotade0@gmail.com>
2023-04-22 08:40:18 +0100

    commit smart contract initialized

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import '@/styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+export const theme = extendTheme({ colors })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider >
        <Component {...pageProps} />
     </ChakraProvider>
   )
