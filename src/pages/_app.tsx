75e80fbe2b83907c3227f2e2e69c12d422704410
Joseph Omotade <josephomotade0@gmail.com>
2023-04-29 07:50:29 +0100

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { useHuddle01 } from '@huddle01/react';
+import { useEffect } from 'react';
 
 const colors = {
   brand: {
@@ -12,7 +14,14 @@ const colors = {
 
 export const theme = extendTheme({ colors })
 
+//const PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID
+
 export default function App({ Component, pageProps }: AppProps) {
+  const { initialize, isInitialized } = useHuddle01();
+  console.log(isInitialized);
+  useEffect(() => {
+   initialize("KL1r3E1yHfcrRbXsT4mcE-3mK60Yc3YR");
+  }, [initialize])
   return (
     <ChakraProvider >
        <Component {...pageProps} />
