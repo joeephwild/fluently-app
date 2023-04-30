db22bd181451b46586ff250e63153b48f45ec3f4
Joseph Omotade <josephomotade0@gmail.com>
2023-04-30 01:21:31 +0100

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,41 +1,30 @@
-import '@/styles/globals.css'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import type { AppProps } from 'next/app'
-import { useHuddle01 } from '@huddle01/react';
-import { useEffect } from 'react';
+import "@/styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { useHuddle01 } from "@huddle01/react";
+import { useEffect } from "react";
 
 const colors = {
   brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
   },
-}
+};
 
-export const theme = extendTheme({ colors })
+export const theme = extendTheme({ colors });
 
 //const PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID
 
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-}
-
-export const theme = extendTheme({ colors })
-
 export default function App({ Component, pageProps }: AppProps) {
   const { initialize, isInitialized } = useHuddle01();
-  console.log(isInitialized);
+  
   useEffect(() => {
-   initialize("KL1r3E1yHfcrRbXsT4mcE-3mK60Yc3YR");
-  }, [initialize])
+    initialize("KL1r3E1yHfcrRbXsT4mcE-3mK60Yc3YR");
+  }, [initialize]);
   return (
-    <ChakraProvider >
-       <Component {...pageProps} />
+    <ChakraProvider>
+      <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
-
