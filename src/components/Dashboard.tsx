ddf94ed77d23af5224c0acd844b018eb9a097565
ambiti0n <fabulousfresh010@gmail.com>
2023-05-03 00:09:51 +0100

    chore: wallet integration

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,85 +1,111 @@
-import { Box, Button, Heading, TableContainer, Table, Thead, Tbody, Td, Th, Tr, Text, Flex, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Heading,
+  TableContainer,
+  Table,
+  Thead,
+  Tbody,
+  Td,
+  Th,
+  Tr,
+  Text,
+  Flex,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useAddress } from "@thirdweb-dev/react";
 import Setup from "./modal/Setup";
+import truncateEthAddress from "truncate-eth-address";
 
 const Dashboard = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure() 
+  const address = useAddress();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    return (
-        <>
-            <Setup isOpen={isOpen} onClose={onClose} />
-            <Box>
-                <Text color='#F0C400' fontWeight={500}>Hey Johnson</Text>
-                <Text my='.5em' fontWeight={700}>Your language partner is waiting for you.</Text>
-                
-                <Button 
-                    mt='1em'
-                    px='1.5em'
-                    bg='#FDD835'
-                    fontSize='.9em'
-                    fontWeight={700}
-                    _hover={{ bg: "#fbdb4f" }}
-                    onClick={onOpen}
-                >
-                    Start Matching Now
-                </Button>
-                
+  return (
+    <>
+      <Setup isOpen={isOpen} onClose={onClose} />
+      <Box>
+        <Text color="#F0C400" fontWeight={500}>
+          Hey {address && `${truncateEthAddress(address)}`}
+        </Text>
+        <Text my=".5em" fontWeight={700}>
+          Your language partner is waiting for you.
+        </Text>
 
-                <Box mt='3em' bg='#FFFDF6' borderRadius={12} p='1em'>
-                    <Text fontSize='1.5em' fontWeight={700} mb='1em'>Upcoming session</Text>
-                    <TableContainer borderRadius={12}>
-                        <Table>
-                            <Thead bg='#FFF7D1'>
-                                <Tr>
-                                    <Th>Date & Time scheduled</Th>
-                                    <Th>Spoken language</Th>
-                                    <Th>Language to learn </Th>
-                                    <Th></Th>
-                                </Tr>
-                            </Thead>
-                            <Tbody fontSize='.9em'>
-                                <Tr>
-                                    <Td>Thurs, 27th April, 4:00 PM</Td>
-                                    <Td>Irish</Td>
-                                    <Td>Pakistan</Td>
-                                    <Td>
-                                        <Flex gap={4}>
-                                            <Button fontSize='.9em' bg='#FDD835'>Join Session</Button>
-                                            <Button 
-                                                fontSize='.9em' 
-                                                color='#FDD835' 
-                                                border='1px solid #FDD835'
-                                                bg='transparent'
-                                            >
-                                                Reschedule
-                                            </Button>
-                                        </Flex>
-                                    </Td>
-                                </Tr>
-                                <Tr>
-                                    <Td>Thurs, 27th April, 4:00 PM</Td>
-                                    <Td>Irish</Td>
-                                    <Td>Pakistan</Td>
-                                    <Td>
-                                        <Flex gap={4}>
-                                            <Button fontSize='.9em' bg='#FDD835'>Join Session</Button>
-                                            <Button 
-                                                fontSize='.9em' 
-                                                color='#FDD835' 
-                                                border='1px solid #FDD835'
-                                                bg='transparent'
-                                            >
-                                                Reschedule
-                                            </Button>
-                                        </Flex>
-                                    </Td>
-                                </Tr>
-                            </Tbody>
-                        </Table>
-                    </TableContainer>
-                </Box>
-            </Box>
-        </>
-    )
-}
+        <Button
+          mt="1em"
+          px="1.5em"
+          bg="#FDD835"
+          fontSize=".9em"
+          fontWeight={700}
+          _hover={{ bg: "#fbdb4f" }}
+          onClick={onOpen}
+        >
+          Start Matching Now
+        </Button>
+
+        <Box mt="3em" bg="#FFFDF6" borderRadius={12} p="1em">
+          <Text fontSize="1.5em" fontWeight={700} mb="1em">
+            Upcoming session
+          </Text>
+          <TableContainer borderRadius={12}>
+            <Table>
+              <Thead bg="#FFF7D1">
+                <Tr>
+                  <Th>Date & Time scheduled</Th>
+                  <Th>Spoken language</Th>
+                  <Th>Language to learn </Th>
+                  <Th></Th>
+                </Tr>
+              </Thead>
+              <Tbody fontSize=".9em">
+                <Tr>
+                  <Td>Thurs, 27th April, 4:00 PM</Td>
+                  <Td>Irish</Td>
+                  <Td>Pakistan</Td>
+                  <Td>
+                    <Flex gap={4}>
+                      <Button fontSize=".9em" bg="#FDD835">
+                        Join Session
+                      </Button>
+                      <Button
+                        fontSize=".9em"
+                        color="#FDD835"
+                        border="1px solid #FDD835"
+                        bg="transparent"
+                      >
+                        Reschedule
+                      </Button>
+                    </Flex>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>Thurs, 27th April, 4:00 PM</Td>
+                  <Td>Irish</Td>
+                  <Td>Pakistan</Td>
+                  <Td>
+                    <Flex gap={4}>
+                      <Button fontSize=".9em" bg="#FDD835">
+                        Join Session
+                      </Button>
+                      <Button
+                        fontSize=".9em"
+                        color="#FDD835"
+                        border="1px solid #FDD835"
+                        bg="transparent"
+                      >
+                        Reschedule
+                      </Button>
+                    </Flex>
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Box>
+    </>
+  );
+};
 
 export default Dashboard;
--- src/components/SignupOption.tsx
+++ src/components/SignupOption.tsx
@@ -1,27 +1,47 @@
-import { Button, Flex, Input, InputGroup, InputRightElement, Tab, TabIndicator, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react"
+import {
+  Button,
+  Flex,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Tab,
+  TabIndicator,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
 import { ConnectWallet, MetaMask } from "./ButtonIcon";
-import { BsExclamation } from 'react-icons/bs'
+import { BsExclamation } from "react-icons/bs";
 import Link from "next/link";
+import {
+  useMetamask,
+  useAddress,
+  ConnectWallet as ConnectWeb3Wallet,
+} from "@thirdweb-dev/react";
 
 const SignupOption = () => {
-    return (
-        <Tabs isFitted variant="unstyled">
-            <TabList>
-                <Tab>Wallet</Tab>
-                <Tab>Email</Tab>
-            </TabList>
-            <TabIndicator 
-                mt="-1.5px"
-                height="2px"
-                width='30px'
-                bg="#000"
-                borderRadius="1px"
-            />
-            <TabPanels>
-                <TabPanel textAlign='left'>
-                    <Text fontWeight={400} fontSize={14} ml={0}>Sign up by connecting with any of your wallets</Text>
-                    <Flex align='center' gap='10' my='1em'>
-                        <Button
+  return (
+    <Tabs isFitted variant="unstyled">
+      <TabList>
+        <Tab>Wallet</Tab>
+        <Tab>Email</Tab>
+      </TabList>
+      <TabIndicator
+        mt="-1.5px"
+        height="2px"
+        width="30px"
+        bg="#000"
+        borderRadius="1px"
+      />
+      <TabPanels>
+        <TabPanel textAlign="left">
+          <Text fontWeight={400} fontSize={14} ml={0}>
+            Sign up by connecting with any of your wallets
+          </Text>
+          <Flex align="center" gap="10" my="4em" justifyContent="center">
+            {/* <Button
                             border='1px solid #FDD835'
                             bg='#FBFAF7'
                             w='full'
@@ -41,40 +61,48 @@ const SignupOption = () => {
                             _hover={{ bg: 'transparent' }}
                         >
                             ConnectWallet
-                        </Button>
-                    </Flex>
-                </TabPanel>
-                <TabPanel>
-                    <Text fontWeight={400} fontSize={14} ml={0}>Sign up with Email address</Text>
-                    <Input 
-                        border='1px solid #FFF7D1'
-                        // w='full'
-                        my='.8em'
-                        placeholder="Enter email address"
-                        fontSize={14}
-                    />
-                    <InputGroup>
-                        <Input 
-                            border='1px solid #FFF7D1'
-                            // w='full'
-                            my='.8em'
-                            placeholder="Enter verification code"
-                            fontSize={14}
-                        />
-                        <InputRightElement w='70px' mt='10px'>
-                            <Link href=''>
-                                <Text as='b' color='#FDD835' fontSize={'.8em'}>Get code</Text>
-                            </Link>
-                        </InputRightElement>
-                    </InputGroup>
-                    <Flex align='center'>
-                        <BsExclamation color='#FDD835' />
-                        <Text fontSize='.7em' color='#000'>Press ‘get code’ button to receive verification code sent to your email address</Text>
-                    </Flex>
-                </TabPanel>
-            </TabPanels>
-        </Tabs>
-    )
-}
+                        </Button> */}
+            <ConnectWeb3Wallet />
+          </Flex>
+        </TabPanel>
+        <TabPanel>
+          <Text fontWeight={400} fontSize={14} ml={0}>
+            Sign up with Email address
+          </Text>
+          <Input
+            border="1px solid #FFF7D1"
+            // w='full'
+            my=".8em"
+            placeholder="Enter email address"
+            fontSize={14}
+          />
+          <InputGroup>
+            <Input
+              border="1px solid #FFF7D1"
+              // w='full'
+              my=".8em"
+              placeholder="Enter verification code"
+              fontSize={14}
+            />
+            <InputRightElement w="70px" mt="10px">
+              <Link href="">
+                <Text as="b" color="#FDD835" fontSize={".8em"}>
+                  Get code
+                </Text>
+              </Link>
+            </InputRightElement>
+          </InputGroup>
+          <Flex align="center">
+            <BsExclamation color="#FDD835" />
+            <Text fontSize=".7em" color="#000">
+              Press ‘get code’ button to receive verification code sent to your
+              email address
+            </Text>
+          </Flex>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
+};
 
 export default SignupOption;
